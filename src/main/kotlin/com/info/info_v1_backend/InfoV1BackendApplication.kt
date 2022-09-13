96e357b5dd6a2f39a9a0a8c8998504a1ea544653
jinu <jinwoo794533@gmail.com>
2022-09-13 19:35:53 +0900

    init

---
+++ src/main/kotlin/com/info/info_v1_backend/InfoV1BackendApplication.kt
@@ -0,0 +1,11 @@
+package com.info.info_v1_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InfoV1BackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<InfoV1BackendApplication>(*args)
+}
