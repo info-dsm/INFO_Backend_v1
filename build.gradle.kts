9a44f6e5743e017f2ff9ca41703050a2cc661c1d
jinu <jinwoo794533@gmail.com>
2022-09-15 08:00:06 +0900

    add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,23 +5,67 @@ plugins {
     id("io.spring.dependency-management") version "1.0.13.RELEASE"
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21"
+    kotlin("plugin.jpa") version "1.6.21"
 }
 
 group = "com.info"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
+
+allOpen {
+    annotation("javax.persistence.Entity")
+    annotation("org.springframework.data.redis.core.RedisHash")
+}
+
+noArg {
+    annotation("javax.persistence.Entity")
+    annotation("org.springframework.data.redis.core.RedisHash")
+}
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter")
+    //WebStarter
+    implementation("org.springframework.boot:spring-boot-starter-web"){
+        exclude(module = "spring-boot-starter-tomcat")
+    }
+    //undertow
+    implementation("org.springframework.boot:spring-boot-starter-undertow")
+    //batch
+    implementation("org.springframework.boot:spring-boot-starter-batch")
+    //Mysql
+    runtimeOnly("mysql:mysql-connector-java")
+//    //Komoran
+//    implementation("com.github.shin285:KOMORAN:3.3.4")
+    //Validation
+    implementation("org.springframework.boot:spring-boot-starter-validation:2.7.3")
+    //AWS
+    implementation("org.springframework.cloud:spring-cloud-starter-aws:2.2.6.RELEASE")
+    //Security
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    //Jwts
+    implementation("io.jsonwebtoken:jjwt:0.9.1")
+    //data jpa
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    //Jackson
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    //mailing
+    implementation("org.springframework.boot:spring-boot-starter-mail")
+    //thymeleaf
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    //redis
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    //spring-doc
+    implementation("org.springdoc:springdoc-openapi-ui:1.6.11")
 }
 
 tasks.withType<KotlinCompile> {
@@ -29,6 +73,10 @@ tasks.withType<KotlinCompile> {
         freeCompilerArgs = listOf("-Xjsr305=strict")
         jvmTarget = "17"
     }
+}
+
+tasks.getByName<Jar>("jar") {
+    enabled = false
 }
 
 tasks.withType<Test> {
