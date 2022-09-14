6d178f0137e079a5fba96e0d9475407ef2f51306
jinu <jinwoo794533@gmail.com>
2022-09-15 08:01:41 +0900

    define team entity

---
+++ src/main/kotlin/com/info/info_v1_backend/domain/team/data/Team.kt
@@ -0,0 +1,36 @@
+package com.info.info_v1_backend.domain.team.data
+
+import java.time.LocalDate
+import javax.persistence.CascadeType
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.OneToMany
+
+@Entity
+class Team(
+    name: String,
+    establishedAt: LocalDate,
+    githubLink: String
+) {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null
+
+    @Column(name = "team_name", nullable = false)
+    var name: String = name
+        protected set
+
+    @Column(name = "team_established_at")
+    val establishedAt: LocalDate = establishedAt
+
+    @Column(name = "team_github_link", nullable = true)
+    var githubLink: String? = githubLink
+
+    @OneToMany(cascade = [CascadeType.REMOVE])
+    var affiliation: MutableList<Affiliation> = ArrayList()
+
+
+
+}
