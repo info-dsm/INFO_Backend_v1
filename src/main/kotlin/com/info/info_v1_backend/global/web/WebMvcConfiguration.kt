d08989e67c4a5a8e129215d09551cfd9f8798487
jinu <jinwoo794533@gmail.com>
2022-09-15 08:04:28 +0900

    ignore cors settings

---
+++ src/main/kotlin/com/info/info_v1_backend/global/web/WebMvcConfiguration.kt
@@ -0,0 +1,17 @@
+package com.info.info_v1_backend.global
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebMvcConfiguration: WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("*")
+            .allowedHeaders("*");
+    }
+
+}
