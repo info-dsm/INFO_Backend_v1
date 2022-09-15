f15eb2cbe463487618170394fa45b8d27c1ee365
jinu <jinwoo794533@gmail.com>
2022-09-15 23:35:41 +0900

    QueryDsl settings

---
+++ src/main/kotlin/com/info/info_v1_backend/global/queryDsl/QueryDslConfig.kt
@@ -0,0 +1,19 @@
+package com.info.info_v1_backend.global.queryDsl
+
+import com.querydsl.jpa.impl.JPAQueryFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import javax.persistence.EntityManager
+import javax.persistence.PersistenceContext
+
+@Configuration
+class QueryDslConfig(
+    @PersistenceContext val entityManager: EntityManager
+) {
+
+    @Bean
+    fun queryFactory(): JPAQueryFactory {
+        return JPAQueryFactory(entityManager)
+    }
+
+}
