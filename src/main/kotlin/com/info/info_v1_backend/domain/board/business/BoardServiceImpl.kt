f5830037b881ed7bf7cffa6e68ce44a38927cdcf
jinu <jinwoo794533@gmail.com>
2022-09-17 18:14:42 +0900

    Remove Board Entity

--- src/main/kotlin/com/info/info_v1_backend/domain/board/business/BoardServiceImpl.kt
+++ src/main/kotlin/com/info/info_v1_backend/domain/board/business/BoardServiceImpl.kt
@@ -1,16 +1,16 @@
 package com.info.info_v1_backend.domain.board.business
 
 import com.info.info_v1_backend.domain.board.business.dto.EmployBoardDto
-import com.info.info_v1_backend.domain.board.data.repository.BoardRepository
+import org.springframework.stereotype.Repository
 import org.springframework.stereotype.Service
 
 @Service
 class BoardServiceImpl(
-    private val boardRepository: BoardRepository
 ): BoardService {
 
     override fun getFullBoard(): EmployBoardDto {
-        return boardRepository.getLatestEmployBoard().toEmployBoardDto()
+        //TODO
+        
     }
 
 }
