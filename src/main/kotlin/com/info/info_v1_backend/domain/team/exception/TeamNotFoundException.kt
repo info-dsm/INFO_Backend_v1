5709dc42885ed7912947ae25c7e170600ef14b8d
jinu <jinwoo794533@gmail.com>
2022-09-19 00:05:51 +0900

    remove Team domain

--- src/main/kotlin/com/info/info_v1_backend/domain/team/exception/TeamNotFoundException.kt
+++
@@ -1,7 +0,0 @@
-package com.info.info_v1_backend.domain.team.exception
-
-import com.info.info_v1_backend.global.error.ErrorCode
-import com.info.info_v1_backend.global.error.data.GlobalError
-
-class TeamNotFoundException(data: String): GlobalError(ErrorCode.TEAM_NOT_FOUND, data) {
-}
