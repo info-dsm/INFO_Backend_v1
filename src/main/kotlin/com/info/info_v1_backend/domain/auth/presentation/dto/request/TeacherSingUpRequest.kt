1dc9270b7cc7ea1ec83841531729f9800b802bf3
squall7011 <gurdl7011@dsm.hs.kr>
2022-09-20 18:49:00 +0900

    Edit Error Message to  valid

--- src/main/kotlin/com/info/info_v1_backend/domain/auth/presentation/dto/request/TeacherSingUpRequest.kt
+++ src/main/kotlin/com/info/info_v1_backend/domain/auth/presentation/dto/request/TeacherSingUpRequest.kt
@@ -9,10 +9,10 @@ data class TeacherSingUpRequest(
     @field:Pattern(regexp = "[a-zA-Z\\d+_.]+@dsm\\.hs\\.kr\$", message = "올바른 이메일 형식이 아닙니다.")
     val email: String,
 
-    @field:Size(min = 4, max = 4)
     val emailCheckCode: String,
 
-    @field:Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d~!@#\$%^&*()+|=]{8,30}\$")
+    @field:Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d~!@#\$%^&*()+|=]{8,30}\$",
+        message = "비밀번호는영소문자,대문자,숫자,특수문자 8~30자여야 합니다.")
     val password: String,
 
     @field:NotNull
