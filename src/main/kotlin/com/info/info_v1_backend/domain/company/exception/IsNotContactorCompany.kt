59e07900a3551b39db45060db065698ff4f19656
jinu <jinwoo794533@gmail.com>
2022-09-29 22:30:30 +0900

    Error

--- src/main/kotlin/com/info/info_v1_backend/domain/company/exception/IsNotContactorCompany.kt
+++
@@ -1,7 +0,0 @@
-package com.info.info_v1_backend.domain.company.exception
-
-import com.info.info_v1_backend.global.error.ErrorCode
-import com.info.info_v1_backend.global.error.data.GlobalError
-
-class IsNotContactorCompany(data: String): GlobalError(ErrorCode.IS_NOT_CONTACTOR_COMPANY, data) {
-}
--- src/main/kotlin/com/info/info_v1_backend/domain/company/exception/NotContactorException.kt
+++
@@ -1,7 +0,0 @@
-package com.info.info_v1_backend.domain.company.exception
-
-import com.info.info_v1_backend.global.error.ErrorCode
-import com.info.info_v1_backend.global.error.data.GlobalError
-
-class NotContactorException(data: String): GlobalError(ErrorCode.NOT_CONTACTOR, data) {
-}
