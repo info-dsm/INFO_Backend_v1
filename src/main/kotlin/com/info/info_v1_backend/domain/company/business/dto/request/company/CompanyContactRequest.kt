a6231ef3d5b3ae7ccc205c8922e59ea0302e5ec3
squall7011 <gurdl7011@dsm.hs.kr>
2022-10-04 19:26:51 +0900

    Phone Number regexp구문 추가

--- src/main/kotlin/com/info/info_v1_backend/domain/company/business/dto/request/company/CompanyContactRequest.kt
+++ src/main/kotlin/com/info/info_v1_backend/domain/company/business/dto/request/company/CompanyContactRequest.kt
@@ -9,12 +9,12 @@ data class CompanyContactRequest(
     val contactorName: String,
     val contactorRank: String?,
     @field:Pattern(
-        regexp = "/^(o2|0[0-9]{2})-?([1-9]{1}[0-9]{2,3})-?([0-9]{4})\$/",
+        regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}\$",
         message = "전화번호는 반드시 02 or 0xx-xxxx-xxxx 조합이여야합니다."
     )
     val phoneNumber: String,
     @field:Pattern(
-        regexp = "/^(o2|0[0-9]{2})-?([1-9]{1}[0-9]{2,3})-?([0-9]{4})\$/",
+        regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}\$",
         message = "전화번호는 반드시 02 or 0xx-xxxx-xxxx 조합이여야합니다."
     )
     val contactorPhone: String?,
--- src/main/kotlin/com/info/info_v1_backend/domain/company/business/dto/request/company/EditCompanyContactRequest.kt
+++ src/main/kotlin/com/info/info_v1_backend/domain/company/business/dto/request/company/EditCompanyContactRequest.kt
@@ -6,13 +6,13 @@ data class EditCompanyContactRequest(
     val contactorName: String?,
     val contactorRank: String?,
     @field:Pattern(
-        regexp = "/^(o2|0[0-9]{2})-?([1-9]{1}[0-9]{2,3})-?([0-9]{4})\$/",
-        message = "전화번호는 반드시 02 or 0xx-xxxx-xxxx 조합이여야합니다."
+        regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}\$",
+        message = "전화번호는 반드시 02 or xxx-xxxx-xxxx 조합이여야합니다."
     )
     val phoneNumber: String?,
     @field:Pattern(
-        regexp = "/^(o2|0[0-9]{2})-?([1-9]{1}[0-9]{2,3})-?([0-9]{4})\$/",
-        message = "전화번호는 반드시 02 or 0xx-xxxx-xxxx 조합이여야합니다."
+        regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}\$",
+        message = "전화번호는 반드시 02 or xxx-xxxx-xxxx 조합이여야합니다."
     )
     val contactorPhone: String?,
     val email: String?,
