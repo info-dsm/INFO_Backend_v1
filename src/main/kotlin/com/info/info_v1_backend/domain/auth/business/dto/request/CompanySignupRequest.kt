c014178a9f5c47b6231086ac254e5aec415316a2
poka <khs0826@dsm.hs.kr>
2022-10-06 18:58:03 +0900

    add @field:Valid

--- src/main/kotlin/com/info/info_v1_backend/domain/auth/business/dto/request/CompanySignupRequest.kt
+++ src/main/kotlin/com/info/info_v1_backend/domain/auth/business/dto/request/CompanySignupRequest.kt
@@ -4,11 +4,14 @@ import com.info.info_v1_backend.domain.company.business.dto.request.company.Comp
 import com.info.info_v1_backend.domain.company.business.dto.request.company.CompanyInformationRequest
 import com.info.info_v1_backend.domain.company.business.dto.request.company.CompanyNameRequest
 import com.info.info_v1_backend.domain.company.business.dto.response.company.BusinessAreaResponse
+import javax.validation.Valid
 import javax.validation.constraints.Size
-
 data class CompanySignupRequest(
+    @field:Valid
     val companyNameRequest: CompanyNameRequest,
+    @field:Valid
     val companyInformation: CompanyInformationRequest,
+    @field:Valid
     val companyContact: CompanyContactRequest,
     val businessAreaList: List<String>,
     @field:Size(
