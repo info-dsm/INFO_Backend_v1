ce1b7ddf4e9cf0f47e402b01bdcac95620001cd2
poka <khs0826@dsm.hs.kr>
2022-10-08 18:06:54 +0900

    @Size -> @Pattern

--- src/main/kotlin/com/info/info_v1_backend/domain/company/business/dto/request/company/CompanyInformationRequest.kt
+++ src/main/kotlin/com/info/info_v1_backend/domain/company/business/dto/request/company/CompanyInformationRequest.kt
@@ -2,9 +2,8 @@ package com.info.info_v1_backend.domain.company.business.dto.request.company
 
 import com.info.info_v1_backend.domain.company.data.entity.company.address.AddressInfo
 import com.info.info_v1_backend.domain.company.data.entity.company.embeddable.CompanyInformation
-import java.time.Year
 import javax.validation.constraints.Max
-import javax.validation.constraints.Size
+import javax.validation.constraints.Pattern
 
 
 data class CompanyInformationRequest(
@@ -14,7 +13,7 @@ data class CompanyInformationRequest(
     val establishedAt: Int,
     @field:Max(100000)
     val workerCount: Int,
-    @field:Size(max = 20)
+    @field:Pattern(regexp = "^\\d{1,20}\$")
     val annualSales: Long
 
 ) {
