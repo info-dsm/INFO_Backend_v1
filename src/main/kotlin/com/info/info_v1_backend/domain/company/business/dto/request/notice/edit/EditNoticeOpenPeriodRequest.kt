fbaf77a475dce44f3a53033e1b07c4c21213bbf6
poka <khs0826@dsm.hs.kr>
2022-10-16 16:00:58 +0900

    startDate and endDate nullable

--- src/main/kotlin/com/info/info_v1_backend/domain/company/business/dto/request/notice/edit/EditNoticeOpenPeriodRequest.kt
+++ src/main/kotlin/com/info/info_v1_backend/domain/company/business/dto/request/notice/edit/EditNoticeOpenPeriodRequest.kt
@@ -3,6 +3,6 @@ package com.info.info_v1_backend.domain.company.business.dto.request.notice.edit
 import java.time.LocalDate
 
 data class EditNoticeOpenPeriodRequest(
-    val startDate: LocalDate,
-    val endDate: LocalDate
+    val startDate: LocalDate?,
+    val endDate: LocalDate?
 )
